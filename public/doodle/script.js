1e6069b80565fd69678f2ddd9c48a818dea8d435
Charles Pawling <cpawling@umd.edu>
2021-02-16 22:28:07 -0500

    doodle stuff

---
+++ public/doodle/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()) =>{
+    const grid = document.querySelector(.grid) // picking up grid item //
+    const doodler = document.createElement('div') // just created a new div for the doodler
+
+function createDoodler() {
+    grid.appendChild(doodler)
+    doodler.classList.add('doodler')
+    doodler.style.left = doodlerleftspace + 'px'
+
+}
+
+
+createDoodler()
